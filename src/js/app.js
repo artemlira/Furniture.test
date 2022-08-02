f7ed73d68d113d6468d18dd965bd82048b09fd0d
Lira Artem <artemlira84@gmail.com>
2022-08-02 18:19:27 +0300

    закончил раздел слайдер, реализовал прокрутку слайдов по нажатию на кнопки, перешел к разделу benefits

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,9 @@
 import * as flsFunctions from "./modules/functions.js";
+import * as sliderFunctions from "./modules/slider.js";
 
 flsFunctions.isWebp();
+sliderFunctions.slider();
+
 
 import Swiper, { Navigation, Pagination } from 'swiper';
 
---
+++ src/js/modules/slider.js
@@ -0,0 +1,65 @@
+const sliders = document.querySelector('.slider')
+const allSliders = sliders.querySelectorAll('.slider__item');
+const number = sliders.querySelector('.first__number');
+const images = sliders.querySelectorAll('.image');
+let index = 0;
+
+
+function slider() {
+   sliders.addEventListener('click', (event) => {
+      let target = event.target;
+      if (target.closest('.next')) {
+         index++;
+         if (index > 3) {
+            index = 0;
+         }
+         next(index);
+      }
+      if (target.closest('.prev')) {
+         index--;
+         if (index < 0) {
+            index = 3;
+         }
+         prev(index);
+      }
+      number.innerText = `0${index + 1}`;
+   });
+}
+
+function next(i) {
+   allSliders.forEach(item => {
+      item.classList.add('hidden');
+      item.classList.remove('animate__fadeInRight');
+      item.classList.remove('animate__fadeInLeft');
+   });
+   images.forEach(item => {
+      item.classList.add('hidden');
+      item.classList.remove('animate__backInRight');
+      item.classList.remove('animate__backInLeft');
+   });
+
+   allSliders[i].classList.remove('hidden');
+   allSliders[i].classList.add('animate__fadeInRight');
+   images[i].classList.remove('hidden');
+   images[i].classList.add('animate__backInRight');
+}
+
+function prev(i) {
+   allSliders.forEach(item => {
+      item.classList.add('hidden');
+      item.classList.remove('animate__fadeInRight');
+      item.classList.remove('animate__fadeInLeft');
+   });
+   images.forEach(item => {
+      item.classList.add('hidden');
+      item.classList.remove('animate__fadeInRight');
+      item.classList.remove('animate__backInLeft');
+   });
+
+   allSliders[i].classList.remove('hidden');
+   allSliders[i].classList.add('animate__fadeInLeft');
+   images[i].classList.remove('hidden');
+   images[i].classList.add('animate__backInLeft');
+}
+
+export { slider, next };
