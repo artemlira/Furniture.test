ee281cbbb2a6ef22fe196d3555afdd1ac58a09c5
Lira Artem <artemlira84@gmail.com>
2022-08-12 13:45:44 +0300

    добавил меню бургер в версии tablet

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,12 @@
 import * as flsFunctions from "./modules/functions.js";
 import * as sliderFunctions from "./modules/slider.js";
 import * as changeImage from "./modules/categories.js";
+import * as burgerMenu from "./modules/burger-menu.js";
 
 flsFunctions.isWebp();
 sliderFunctions.slider();
 changeImage.changeImage();
+burgerMenu.showMenu();
 
 
 import Swiper, { Navigation, Pagination } from 'swiper';
