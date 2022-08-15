c64d79579f9d9602edb7aa328b2a1af70e600be8
Lira Artem <artemlira84@gmail.com>
2022-08-15 21:00:37 +0300

    исправил некоторые ошибки в адаптиве, начал делать слайдер на карточках benefits

--- src/js/modules/benefits-slider.js
+++ src/js/modules/benefits-slider.js
@@ -1,8 +1,24 @@
+
 const items = document.querySelector('.benefits__items');
+const item = items.querySelectorAll('.benefits__item');
+
 
 
 
 export function benefitsSlider() {
-   console.dir(items);
+   items.addEventListener('click', (event) => {
+      let target = event.target;
+      if (target.closest('.left')) {
+         item.forEach(e => e.classList.add('hidden'));
+         for (let i = 2; i < item.length; i--) {
+            if (i < 0) {
+               i = 2;
+            }
+            item[i].classList.remove('hidden');
+         }
+      }
+
+   });
+
 }
 
